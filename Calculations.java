1b52c5916c15a086f9231b4f672a740eecd833b5
Lounes <b00074006@aus.edu>
2020-06-12 06:25:47 +0400

    Subtraction implemented

--- Calculations.java
+++ Calculations.java
@@ -3,18 +3,19 @@ public class Calculations {
 	public static void main(String[] args) {
 		int x = 2;
 		int y = 1;
-		
-		//sum of x and y 
+
+		//sum of x and y
 		int sum = x+y;
 		System.out.println("The sum is= "+ sum);
-		
+
 		//member_B add the feature of subtracting x from y and print the result
-		
+		int sub= y-x;
+		System.out.println("The subtraction is= "+ sub);
 		//member_C add the feature of dividing x by y and print the result
-		
-		//member_D add the feature of multiplying x by y and print the result
-		
-		
+
+
+
+
 	}
 
 }
